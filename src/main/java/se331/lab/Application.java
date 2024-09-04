006246c900b6d9d3cdb7f070ff355471ce64a5ad
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-09-04 13:50:24 +0700

    update version "22"

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -2,6 +2,9 @@ package se331.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/events")
+                        .allowedOrigins("http://localhost:5173");
+                registry.addMapping("/events/*")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 }
