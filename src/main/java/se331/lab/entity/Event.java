a2ea699c25bbd0a370c70db448ada788c976616d
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-09-13 22:38:17 +0700

    Lab8 3.1

--- src/main/java/se331/lab/entity/Event.java
+++ src/main/java/se331/lab/entity/Event.java
@@ -1,12 +1,21 @@
 package se331.lab.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Event {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @EqualsAndHashCode.Exclude
     Long id;
     String category;
     String title;
